5fcbb976f34ad85b3b5e3afb012054556ef90513
Cookee24 <190457301+Cookee24@users.noreply.github.com>
2025-11-01 16:37:29 +0800

    refactor: remove timeout of mount

--- entrypoints/content/index.ts
+++ entrypoints/content/index.ts
@@ -5,18 +5,14 @@ import "~/utils/rpc/wxt-def";
 export default defineContentScript({
 	matches: ["<all_urls>"],
 	main: () =>
-		setTimeout(
-			() =>
-				requestIdleCallback(
-					async () => {
-						await rpcReady();
-						const enabled = await window.rpc.isEnabled();
-						if (!enabled) return;
+		requestIdleCallback(
+			async () => {
+				await rpcReady();
+				const enabled = await window.rpc.isEnabled();
+				if (!enabled) return;
 
-						await mountOverlay(App);
-					},
-					{ timeout: 2000 },
-				),
-			500,
+				await mountOverlay(App);
+			},
+			{ timeout: 2000 },
 		),
 });
